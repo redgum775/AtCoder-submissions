77dd1d41c5ecb9a8109901be4ff80bd249c9c21b
redgum <redgum775@gmail.com>
2022-08-27 23:53:43 +0900

    add submission: 2022/08/27 23:53:43

---
+++ submissions/abc266/a.cpp
@@ -0,0 +1,21 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using ll = long long;
+typedef long long ll;
+using llu = unsigned long long;
+typedef unsigned long long llu;
+
+using namespace std;
+
+int main() {
+  string S;
+  cin >> S;
+
+  cout << S[((int)(S.length()) + 1) / 2 - 1] << endl;
+
+  return 0;
+}
---
+++ submissions/abc266/b.cpp
@@ -0,0 +1,26 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using ll = long long;
+typedef long long ll;
+using llu = unsigned long long;
+typedef unsigned long long llu;
+
+using namespace std;
+
+int main() {
+  ll N;
+  cin >> N;
+
+  ll ans = N % 998244353;
+  if(ans < 0) ans += 998244353;
+
+//cout << fixed << setprecision(10); //¬”“_ˆÈ‰º‚Ìo—ÍŒ…”‚ÌŽw’è
+
+  cout << ans << endl;
+
+  return 0;
+}
